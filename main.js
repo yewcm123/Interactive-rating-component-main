4c8a5efb001e7718ba12379cde24d7850ac889c7
yewcm123 <yewcm123@gmail.com>
2022-11-14 21:22:45 +0800

    Changed "div inside label" into proper way to place elements

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 const ratingPage = document.getElementById("rating-page");
 const thankYouPage = document.getElementById("thank-you-page");
-
+const radioButton = document.querySelectorAll(".radio-button")
 const submitRatingButton = document.getElementById("submit-button");
 const ratingValue = document.getElementById("rating-value");
 
 let ratingValueSelected = null;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const ratingRadio = document.getElementById("rating-radio");
-  for (let label of ratingRadio.children) {
-    label
-      .getElementsByTagName("input")[0]
-      .addEventListener("click", RatingClicked);
-  }
-});
-
-const RatingClicked = (e) => {
-  const ratingRadio = document.getElementById("rating-radio")
-  ratingValueSelected = e.target.value;
-  for (let label of ratingRadio.children) {
-    let ratingCheck = label.getElementsByTagName("input")[0];
-    if (ratingCheck.value !== ratingValueSelected) {
-      ratingCheck.checked = false;
+const RatingClicked = (e) => {  
+  ratingValueSelected = e.target.innerText;
+  for (let i = 0; i < radioButton.length; i++) {
+    if (radioButton[i].getElementsByTagName("input")[0].value !== ratingValueSelected)
+    {
+      radioButton[i].classList.remove("selected")
+    }else{
+      radioButton[i].classList.add("selected")
     }
   }
 };
+
+window.addEventListener('DOMContentLoaded', ()=> {
+  for (let button = 0; button < radioButton.length; button++)
+  {
+    radioButton[button].addEventListener("click", RatingClicked)
+  }
+})
+
 
 submitRatingButton.addEventListener("click", (e) => {
   if (ratingValueSelected === null) {
